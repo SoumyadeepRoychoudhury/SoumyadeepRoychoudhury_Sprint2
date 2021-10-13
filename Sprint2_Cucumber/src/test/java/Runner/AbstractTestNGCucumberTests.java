6325fbc0bc2d067160f30940a5ec5c1fa87ff2bc
Deepu <Deepu@DESKTOP-V2309OA>
2021-10-13 11:53:27 +0530

    project

---
+++ Sprint2_Cucumber/src/test/java/Runner/AbstractTestNGCucumberTests.java
@@ -0,0 +1,10 @@
+package Runner;
+
+public class AbstractTestNGCucumberTests {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Sprint2_Cucumber/src/test/java/StepDef/OffersStepDef.java
@@ -0,0 +1,49 @@
+package StepDef;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import com.base.Base;
+import com.pages.Offers;
+
+import io.cucumber.java.After;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class OffersStepDef extends Offers
+{
+
+	Offers Of;
+	
+	@Given("User is in Home Page")
+	public void user_is_in_home_page() 
+	{
+		Of = new Offers();
+		Of.initialization();
+	}
+	    
+	@When("User clicks on Offer button")
+	public void user_clicks_on_offer_button() 
+	{
+		 Of.offerButton();
+	}
+
+	@When("User click on add to cart button")
+	public void user_click_on_add_to_cart_button() 
+	{
+		Of.cartButton();
+	}		
+
+	@When("User clicks on cart\\/basket button")
+	public void user_clicks_on_cart_basket_button() 
+	{
+		 Of.cartBasketButton();
+		 Of.teardown();
+	}	
+
+}
